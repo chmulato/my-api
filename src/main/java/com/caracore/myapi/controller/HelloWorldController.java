f75949fe38ab8a612d292961de87974a64365ac7
Christian Mulato <chmulato@hotmail.com>
2022-12-17 19:17:30 -0300

    Primeiro controller Rest Hello World.

---
+++ src/main/java/com/caracore/myapi/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.caracore.myapi.controller;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld(@RequestParam(required = false) String name) {
+        return "Hello " + (StringUtils.hasText(name) ? name : "World");
+    }
+    
+}
