422d576dd043bb2dca2a5425124fa10be06b3793
Christian Mulato <chmulato@hotmail.com>
2022-12-19 17:01:24 -0300

    Configurando o CORS

--- src/main/java/com/caracore/myapi/MyApiApplication.java
+++ src/main/java/com/caracore/myapi/MyApiApplication.java
@@ -2,12 +2,50 @@ package com.caracore.myapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApiApplication.class, args);
+	}
+
+	@Profile("local")
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("http://localhost:4200")
+					.allowedMethods("GET", "PUT", "OPTIONS", "POST", "DELETE")
+					.maxAge(900)
+					.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization");
+			}
+			
+		};
+	}
+
+	@Profile("aws")
+	@Bean
+	public WebMvcConfigurer corsAwsConfig() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("http://localhost:4200")
+					.allowedMethods("GET", "PUT", "OPTIONS", "POST", "DELETE")
+					.maxAge(900)
+					.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization");
+			}
+			
+		};
 	}
 
 }
