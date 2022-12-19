b6dba0e90bc973be25ce55151cbfd844cdc3931e
Christian Mulato <chmulato@hotmail.com>
2022-12-19 12:06:19 -0300

    Criando uma classe de erro para a API.

--- src/main/java/com/caracore/myapi/ChecklistApiApplication.java
+++ src/main/java/com/caracore/myapi/ChecklistApiApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.caracore.myapi.entities.CategoryEntity;
-import com.caracore.myapi.repository.CategoryRepository;
+import com.caracore.myapi.service.CategoryService;
 
 @SpringBootApplication
 public class ChecklistApiApplication {
@@ -16,26 +15,10 @@ public class ChecklistApiApplication {
 	}
 		
 	@Bean
-	CommandLineRunner initDatabase(CategoryRepository categoryRepository) {
-
+	CommandLineRunner initDatabase(CategoryService categoryService) {
 		return args -> {
-
-			categoryRepository.deleteAll();
-			
-			CategoryEntity cat01 = new CategoryEntity();
-			cat01.setCategoryId(1L);
-			cat01.setGuid("aaa-bbb-ccc-dddd");
-			cat01.setName("Saude");
-			
-			categoryRepository.save(cat01);
-
-			CategoryEntity cat02 = new CategoryEntity();
-			cat02.setCategoryId(2L);
-			cat02.setGuid("aaa-bbb-ccc-dddf");
-			cat02.setName("Trabalho");
-			
-			categoryRepository.save(cat02);
-			
+			categoryService.addNewCategory("Saude");
+			categoryService.addNewCategory("Trabalho");
 		};
 	}	
 
--- src/main/java/com/caracore/myapi/exception/CustomExceptionHandler.java
+++ src/main/java/com/caracore/myapi/exception/CustomExceptionHandler.java
@@ -1,9 +1,20 @@
 package com.caracore.myapi.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
     
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<Object> handleResourceNotFoundException(Exception ex, WebRequest request) throws Exception {
+        log.error("An error happend to call API: {}", ex);
+        return this.handleException(ex, request);
+    }
 }
