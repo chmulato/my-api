402a4555304aa290eba2655a2fb048e98071d7fb
Christian Mulato <chmulato@hotmail.com>
2022-12-19 10:00:02 -0300

    Criando o controller REST ChecklistItem.

---
+++ src/main/java/com/caracore/myapi/controller/ChecklistItemController.java
@@ -0,0 +1,80 @@
+package com.caracore.myapi.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caracore.myapi.dto.ChecklistItemDTO;
+import com.caracore.myapi.entities.ChecklistItemEntity;
+import com.caracore.myapi.exception.ValidationException;
+import com.caracore.myapi.service.ChecklistItemService;
+
+@RestController
+@RequestMapping("/api/v1/checklist-items")
+public class ChecklistItemController {
+
+    private ChecklistItemService checklistItemService;
+    
+    public ChecklistItemController(ChecklistItemService checklistItemService) {
+        this.checklistItemService = checklistItemService;
+    }
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ChecklistItemDTO>> getAllChecklistItems() {
+
+        List<ChecklistItemDTO> resp = StreamSupport.stream(this.checklistItemService.findAllChecklistItems().spliterator(), false)
+        .map(checklistItemEntity -> ChecklistItemDTO.toDTO(checklistItemEntity)).collect(Collectors.toList());
+
+        return new ResponseEntity<>(resp, HttpStatus.OK);
+    
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createNewChecklistItem(@RequestBody ChecklistItemDTO checklistItemDTO) {
+
+        if (checklistItemDTO.getCategory() == null) {
+            throw new ValidationException("Category cannot null");
+        }
+
+        ChecklistItemEntity newChecklistItem = this.checklistItemService.addNewChecklistItem(
+            checklistItemDTO.getDescription(), checklistItemDTO.getIsCompleted(),
+            checklistItemDTO.getDeadline(), checklistItemDTO.getCategory().getGuid());
+
+        return new ResponseEntity<>(newChecklistItem.getGuid(), HttpStatus.CREATED);
+    
+    }
+
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateChecklistItem(@RequestBody ChecklistItemDTO checklistItemDTO) {
+
+        if (!StringUtils.hasLength(checklistItemDTO.getGuid())) {
+            throw new ValidationException("Checklist item is mandatory");
+        }
+
+        this.checklistItemService.updateChecklistItem(checklistItemDTO.getGuid(), checklistItemDTO.getDescription(),
+            checklistItemDTO.getIsCompleted(), checklistItemDTO.getDeadline(),
+            checklistItemDTO.getCategory() != null ? checklistItemDTO.getCategory().getGuid() : null)
+        
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping(value = "{guid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> deleteChecklistItem(@PathVariable String guid) {
+        this.checklistItemService.deleteChecklistItem(guid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
