22a3604821321124aab992b09a4babfaad4f73a2
Christian Mulato <chmulato@hotmail.com>
2022-12-19 14:48:56 -0300

    Ajustando a classe de mock do banco de dados local

---
+++ src/main/java/com/caracore/myapi/MyApiApplication.java
@@ -0,0 +1,13 @@
+package com.caracore.myapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiApplication.class, args);
+	}
+
+}
