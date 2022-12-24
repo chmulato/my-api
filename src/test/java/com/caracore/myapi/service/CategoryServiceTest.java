f39ff6b54e12bafa93d40e11269a9c7571fdfee4
Christian Mulato <chmulato@hotmail.com>
2022-12-24 12:48:07 -0300

    Classe de teste unitário para categoria service.

---
+++ src/test/java/com/caracore/myapi/service/CategoryServiceTest.java
@@ -0,0 +1,69 @@
+package com.caracore.myapi.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.caracore.myapi.MyApiApplication;
+import com.caracore.myapi.entities.CategoryEntity;
+import com.caracore.myapi.repository.CategoryRepository;
+import com.caracore.myapi.repository.ChecklistItemRepository;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = MyApiApplication.class)
+public class CategoryServiceTest {
+    
+    
+    private CategoryService categoryService;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
+    private ChecklistItemRepository checklistItemRepository;
+
+    @BeforeEach
+    public void initTest() {
+        this.categoryService = new CategoryService(checklistItemRepository, categoryRepository);
+    }
+
+    @Test
+    public void shouldCreateCategorySuccessfully() {
+
+        //having
+        String categoryName = "Personal";
+
+        //when
+        when(categoryRepository.save(any(CategoryEntity.class))).thenReturn(new CategoryEntity());
+
+        CategoryEntity categoryEntity = this.categoryService.addNewCategory(categoryName);
+
+        //then
+        Assertions.assertNotNull(categoryEntity);
+        verify(categoryRepository, times(1)).save(
+            argThat(categoryEntityArg -> categoryEntityArg.getName().equals("Personal")
+            && categoryEntityArg.getGuid() != null)
+        );
+    }
+
+    @Test
+    public void shouldThrownAnExceptionWhenCategoryNameIsEmptyOrNull() {
+
+        Exception exception =
+            Assertions.assertThrows(IllegalArgumentException.class, () -> this.categoryService.addNewCategory(null));
+
+            Assertions.assertEquals("Category name cannot be empty or null", exception.getMessage());
+    
+    }
+
+}
