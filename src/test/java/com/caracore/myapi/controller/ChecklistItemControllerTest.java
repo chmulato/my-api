f18556bb5d2722a5ff81382862b663745f860b6e
Christian Mulato <chmulato@hotmail.com>
2022-12-24 16:47:47 -0300

    Ajustando os import.

--- src/test/java/com/caracore/myapi/controller/ChecklistItemControllerTest.java
+++ src/test/java/com/caracore/myapi/controller/ChecklistItemControllerTest.java
@@ -17,11 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.caracore.myapi.dto.CategoryDTO;
-import com.caracore.myapi.dto.ChecklistItemDTO;
 import com.caracore.myapi.entities.CategoryEntity;
 import com.caracore.myapi.entities.ChecklistItemEntity;
 import com.caracore.myapi.service.ChecklistItemService;
