bb99eca3bbec46d1c560dc9f17370abcd7bce6d8
Christian Mulato <chmulato@hotmail.com>
2022-12-23 22:09:01 -0300

    Ajuste dos getters e setters

--- src/main/java/com/caracore/myapi/dto/GuidDTO.java
+++ src/main/java/com/caracore/myapi/dto/GuidDTO.java
@@ -1,8 +1,14 @@
 package com.caracore.myapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GuidDTO {
     
     private String guid;
