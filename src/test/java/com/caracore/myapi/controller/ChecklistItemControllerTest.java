44b44b11fe41c6535c7ebe59b08c5c9c5924d91b
Christian Mulato <chmulato@hotmail.com>
2022-12-24 18:38:42 -0300

    Testes unitários REST ChecklistItem.

--- src/test/java/com/caracore/myapi/controller/ChecklistItemControllerTest.java
+++ src/test/java/com/caracore/myapi/controller/ChecklistItemControllerTest.java
@@ -1,7 +1,16 @@
 package com.caracore.myapi.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.hasSize;
@@ -16,12 +25,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.caracore.myapi.dto.CategoryDTO;
+import com.caracore.myapi.dto.ChecklistItemDTO;
+import com.caracore.myapi.dto.UpdateStatusDTO;
 import com.caracore.myapi.entities.CategoryEntity;
 import com.caracore.myapi.entities.ChecklistItemEntity;
 import com.caracore.myapi.service.ChecklistItemService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(ChecklistItemController.class)
 public class ChecklistItemControllerTest {
@@ -31,6 +45,31 @@ public class ChecklistItemControllerTest {
     
     @MockBean
     private ChecklistItemService checklistItemService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    private UpdateStatusDTO getStatusDTO(Boolean isCompleted) {
+        UpdateStatusDTO statusDTO = new UpdateStatusDTO();
+        statusDTO.setComplete(isCompleted);
+        return statusDTO;
+    }
+
+    private ChecklistItemDTO getChecklistItemDTO(String description, Boolean isCompleted, LocalDate deadline,
+                                                String categoryName) {
+        return ChecklistItemDTO.builder()
+        .guid(UUID.randomUUID().toString())
+        .description(description)
+        .isCompleted(isCompleted)
+        .deadline(deadline)
+        .category(CategoryDTO.builder()
+            .guid(UUID.randomUUID().toString())
+            .name(categoryName)
+        .build())
+        .postedDate(LocalDate.now())
+        .build();
+    }
 
     private ChecklistItemEntity getChecklistItemEntity(Long id, String description, Boolean isCompleted,
     LocalDate deadline, Long categoryId, String categoryName) {
@@ -68,9 +107,104 @@ public class ChecklistItemControllerTest {
         .andExpect(status().isOk())
         .andExpect(MockMvcResultMatchers.jsonPath("$[*]", hasSize(2)))
         .andExpect(MockMvcResultMatchers.jsonPath("$[0].guid").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("Item 1"))
         .andExpect(MockMvcResultMatchers.jsonPath("$[0].isCompleted").value(false))
-        .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("Item 1"));
-    
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].deadline").value("2022-10-01"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].postedDate").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].category").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].category.guid").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].category.name").value("Cat 1"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].guid").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].description").value("Item 2"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].isCompleted").value(false))
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].deadline").value("2022-10-02"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].postedDate").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].category").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].category.guid").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[1].category.name").value("Cat 2")); 
     }
 
+    @Test
+    public void shouldCallEndpointAndAddNewChecklistItemAndReturn201() throws Exception {
+
+        //having
+        when(this.checklistItemService.addNewChecklistItem(anyString(),
+         anyBoolean(), any(LocalDate.class), anyString()))
+            .thenReturn(
+                getChecklistItemEntity(1L, "Item 1", false, LocalDate.of(2022, 10, 01), 1L ,"Cat 1"));
+
+        //when - then
+        this.mockMvc.perform(post("/v1/api/checklist-items")
+        .content(objectMapper.writeValueAsString(
+            getChecklistItemDTO(
+                "Test",
+                true,
+                LocalDate.of(2022, 12, 24),
+                "Test Cat")
+        ))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.guid").isNotEmpty());
+
+    }
+
+    @Test
+    public void shouldCallEndpointAndAddUpdateChecklistItemAndReturn204() throws Exception {
+
+        //having
+        when(this.checklistItemService.addNewChecklistItem(anyString(),
+         anyBoolean(), any(LocalDate.class), anyString()))
+            .thenReturn(
+                getChecklistItemEntity(1L, "Item 1", false, LocalDate.of(2022, 10, 01), 1L ,"Cat 1"));
+
+        //when - then
+        this.mockMvc.perform(put("/v1/api/checklist-items")
+        .content(objectMapper.writeValueAsString(
+            getChecklistItemDTO(
+                "Test",
+                true,
+                LocalDate.of(2022, 12, 24),
+                "Test Cat")
+        ))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void shouldCallEndpointAndAddDeleteChecklistItemAndReturn204() throws Exception {
+
+        //having
+        String checklistItemGuid = UUID.randomUUID().toString();
+
+        //when - then
+        this.mockMvc.perform(delete("/v1/api/checklist-items/{guid}", checklistItemGuid)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isNoContent());
+
+        verify(checklistItemService, times(1)).deleteChecklistItem(checklistItemGuid);
+
+    }
+
+    @Test
+    public void shouldCallEndpointAndAddPatchChecklistItemAndReturn202() throws Exception {
+
+        //having
+        String checklistItemGuid = UUID.randomUUID().toString();
+
+        //when - then
+        this.mockMvc.perform(patch("/v1/api/checklist-items/{guid}", checklistItemGuid)
+        .content(objectMapper.writeValueAsString(
+            getStatusDTO(true)
+        ))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isAccepted());
+
+        verify(checklistItemService, times(1)).updateIsCompleteStatus(checklistItemGuid, true);
+
+    }
 }
