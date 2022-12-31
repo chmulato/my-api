210cdfa04b44a61509d51f1cc1f2abbfc3cf580b
Christian Mulato <chmulato@hotmail.com>
2022-12-30 23:23:03 -0300

    Config mysql local com docker

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into Category (category_id, guid, name) values (1,'968deed9-3106-44a4-b9c0-c35b0085ca80','Trabalho');
+insert into Category (category_id, guid, name) values (2,'9043910e-0e48-4a3a-bdc9-b6c63fe0d676','Saude');
+insert into Category (category_id, guid, name) values (3,'01428a2e-4121-4754-9f1d-f8a2b8c59a52','Educacao');
+insert into Category (category_id, guid, name) values (4,'fd6be109-dfbb-45f4-95b3-422027d7b1bb','Pessoal');
+insert into Category (category_id, guid, name) values (5,'56866091-b9af-4c4c-b5d0-6841267ea65a','Outros');
