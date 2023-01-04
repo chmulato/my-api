8cea51f1e1ac35b06ed5f2326d754608ae09b47a
Christian Mulato <chmulato@hotmail.com>
2023-01-04 17:03:59 -0300

    WAR para Tomcat com Java 8

--- src/main/java/com/caracore/myapi/MyApiApplication.java
+++ src/main/java/com/caracore/myapi/MyApiApplication.java
@@ -2,12 +2,13 @@ package com.caracore.myapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MyApiApplication {
+public class MyApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApiApplication.class, args);
