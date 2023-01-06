cfe0f75163df5e573e4ec53452735f9f9d0729e5
Christian Mulato <chmulato@hotmail.com>
2023-01-05 22:26:23 -0300

    Deploy em ambiente de produção.

--- src/main/java/com/caracore/myapi/MyApiApplication.java
+++ src/main/java/com/caracore/myapi/MyApiApplication.java
@@ -3,32 +3,12 @@ package com.caracore.myapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApiApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfig() {
-		return new WebMvcConfigurer() {
-
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedOrigins(
-					"http://www.caracore.com.br", "http://209.145.49.81")
-					.allowedMethods("GET", "PUT", "OPTIONS", "POST", "DELETE", "PATCH")
-					.maxAge(900)
-					.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization");
-			}
-			
-		};
 	}
 
 }
